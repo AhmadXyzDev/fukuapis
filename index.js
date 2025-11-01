70ecb7589b5dd801b843dd68ba090407ae012df6
AhmadXyzDev <reyzaaffreyzafaizramadhan@gmail.com>
2025-11-01 15:18:16 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 3000;
 
